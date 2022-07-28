09ff55ca0e00df51f23fc7c73823f11a2ee48537
Voke <Vokeko@MEOWWARE>
2022-07-28 21:39:04 +0200

    Menu

--- Program.cs
+++ Program.cs
@@ -10,25 +10,22 @@ namespace Beeper
     {
         static void Main(string[] args)
         {
-            Noty note = new Noty();
-            Console.ReadKey();
-            Console.Beep(note.notyNaFrekvenci[Noty.nota.C4], 300);
-            Console.Beep(note.notyNaFrekvenci[Noty.nota.D4], 300);
-            Console.Beep(note.notyNaFrekvenci[Noty.nota.E4], 300);
-            Console.Beep(note.notyNaFrekvenci[Noty.nota.F4], 300);
-            Console.Beep(note.notyNaFrekvenci[Noty.nota.G4], 300);
-            Console.Beep(note.notyNaFrekvenci[Noty.nota.A4], 300);
-            Console.Beep(note.notyNaFrekvenci[Noty.nota.H4], 300);
-            Console.Beep(note.notyNaFrekvenci[Noty.nota.C5], 300);
-            Console.ReadKey();
-        }
-
-        public class Noty
-        {
-            public Dictionary<nota, int> notyNaFrekvenci { get; }
-            public Noty()
+            Console.Title = "Beeper - pípací skladatel";
+            string[] polozky = new string[5] { "Nová písnička", "Import", "Hrát písničku", "Info", "Odejít" };
+            bool exit = false;
+            while (!exit)
             {
-                notyNaFrekvenci = new Dictionary<nota, int>()
+                exit = Menu.nabidka(polozky);
+            }
+        }
+        
+    }
+    class Noty
+    {
+        public Dictionary<nota, int> notyNaFrekvenci { get; }
+        public Noty()
+        {
+            notyNaFrekvenci = new Dictionary<nota, int>()
                 {
                     {nota.D1,37 },{nota.Eb1,39 },{ nota.E1, 41 },{nota.F1,44 },{nota.Gb1,46 },{nota.G1,49 },{nota.Ab1,52 }, {nota.A1,55 }, {nota.Hb1,58 }, {nota.H1,62 },
                          {nota.C2,65 },{nota.Db2,70 },{nota.D2,73 },{nota.Eb2,78 },{ nota.E2, 82 },{nota.F2,87 },{nota.Gb2,92 },{nota.G2,100 },{nota.Ab2,104 }, {nota.A2,110 }, {nota.Hb2,117 }, {nota.H2,123 },
@@ -39,104 +36,188 @@ namespace Beeper
                          {nota.C7,2093 },{nota.Db7,2217 },{nota.D7,2349 },{nota.Eb7,2489 },{ nota.E7, 2637 },{nota.F7,2794 },{nota.Gb7,2960 },{nota.G7,3136 },{nota.Ab7,3322 }, {nota.A7,3520 }, {nota.Hb7,3729 }, {nota.H7,3951 },
                          {nota.C8,4186 },{nota.Db8,4435 },{nota.D8,4699 },{nota.Eb8,4978 },{ nota.E8, 5274 },{nota.F8,5588 },{nota.Gb8,5919 },{nota.G8,6272 },{nota.Ab8,6645 }, {nota.A8,7040 }, {nota.Hb8,7459 }, {nota.H8,7902 },
                 };
-            }
-            public enum nota
+        }
+        public enum nota
+        {
+            D1,
+            Eb1,
+            E1,
+            F1,
+            Gb1,
+            G1,
+            Ab1,
+            A1,
+            H1,
+            Hb1,
+            C2,
+            Db2,
+            D2,
+            Eb2,
+            E2,
+            F2,
+            Gb2,
+            G2,
+            Ab2,
+            A2,
+            Hb2,
+            H2,
+            C3,
+            Db3,
+            D3,
+            Eb3,
+            E3,
+            F3,
+            Gb3,
+            G3,
+            Ab3,
+            A3,
+            Hb3,
+            H3,
+            C4,
+            Db4,
+            D4,
+            Eb4,
+            E4,
+            F4,
+            Gb4,
+            G4,
+            Ab4,
+            A4,
+            Hb4,
+            H4,
+            C5,
+            Db5,
+            D5,
+            Eb5,
+            E5,
+            F5,
+            Gb5,
+            G5,
+            Ab5,
+            A5,
+            Hb5,
+            H5,
+            C6,
+            Db6,
+            D6,
+            Eb6,
+            E6,
+            F6,
+            Gb6,
+            G6,
+            Ab6,
+            A6,
+            Hb6,
+            H6,
+            C7,
+            Db7,
+            D7,
+            Eb7,
+            E7,
+            F7,
+            Gb7,
+            G7,
+            Ab7,
+            A7,
+            Hb7,
+            H7,
+            C8,
+            Db8,
+            D8,
+            Eb8,
+            E8,
+            F8,
+            Gb8,
+            G8,
+            Ab8,
+            A8,
+            Hb8,
+            H8,
+        }
+    }
+
+    public static class Menu
+    {
+        public static bool nabidka(string[] polozky)
+        {
+            int zvolenaPolozka = 0;
+            bool vyberdokoncen = false;
+            Console.Clear();
+            while (!vyberdokoncen)
             {
-                D1,
-                Eb1,
-                E1,
-                F1,
-                Gb1,
-                G1,
-                Ab1,
-                A1,
-                H1,
-                Hb1,
-                C2,
-                Db2,
-                D2,
-                Eb2,
-                E2,
-                F2,
-                Gb2,
-                G2,
-                Ab2,
-                A2,
-                Hb2,
-                H2,
-                C3,
-                Db3,
-                D3,
-                Eb3,
-                E3,
-                F3,
-                Gb3,
-                G3,
-                Ab3,
-                A3,
-                Hb3,
-                H3,
-                C4,
-                Db4,
-                D4,
-                Eb4,
-                E4,
-                F4,
-                Gb4,
-                G4,
-                Ab4,
-                A4,
-                Hb4,
-                H4,
-                C5,
-                Db5,
-                D5,
-                Eb5,
-                E5,
-                F5,
-                Gb5,
-                G5,
-                Ab5,
-                A5,
-                Hb5,
-                H5,
-                C6,
-                Db6,
-                D6,
-                Eb6,
-                E6,
-                F6,
-                Gb6,
-                G6,
-                Ab6,
-                A6,
-                Hb6,
-                H6,
-                C7,
-                Db7,
-                D7,
-                Eb7,
-                E7,
-                F7,
-                Gb7,
-                G7,
-                Ab7,
-                A7,
-                Hb7,
-                H7,
-                C8,
-                Db8,
-                D8,
-                Eb8,
-                E8,
-                F8,
-                Gb8,
-                G8,
-                Ab8,
-                A8,
-                Hb8,
-                H8,
+                Console.SetCursorPosition(0, 3);
+                for (int I = 0; I < polozky.Length; I++)
+                {
+                    if (zvolenaPolozka == I)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                    }
+                    Console.WriteLine(polozky[I]);
+                    Console.ResetColor();
+                }
+                ConsoleKeyInfo stisknutaKlavesa = Console.ReadKey(true);
+                switch (stisknutaKlavesa.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        zvolenaPolozka--;
+                        if (zvolenaPolozka < 0)
+                        {
+                            zvolenaPolozka = polozky.Length - 1;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        zvolenaPolozka++;
+                        if (zvolenaPolozka > polozky.Length - 1)
+                        {
+                            zvolenaPolozka = 0;
+                        }
+                        break;
+                    case ConsoleKey.Enter:
+                        vyberdokoncen = true;
+                        break;
+                    default:
+                        break;
+                }
             }
+            return getFromNabidka(polozky[zvolenaPolozka]);
+        }
+
+        private static bool getFromNabidka(string polozka)
+        {
+            switch(polozka)
+            {
+                case "Nová písnička":
+                    Console.Clear();
+                    Console.WriteLine("Funkce není naimplentována");
+                    Console.ReadKey();
+                    return false;
+                case "Import":
+                    Console.Clear();
+                    Console.WriteLine("Funkce není naimplentována");
+                    Console.ReadKey();
+                    return false;
+                case "Hrát písničku":
+                    Noty note = new Noty();
+                    Console.Clear();
+                    Console.WriteLine("Hraju :)");
+                    Console.Beep(note.notyNaFrekvenci[Noty.nota.C4], 300);
+                    Console.Beep(note.notyNaFrekvenci[Noty.nota.D4], 300);
+                    Console.Beep(note.notyNaFrekvenci[Noty.nota.E4], 300);
+                    Console.Beep(note.notyNaFrekvenci[Noty.nota.F4], 300);
+                    Console.Beep(note.notyNaFrekvenci[Noty.nota.G4], 300);
+                    Console.Beep(note.notyNaFrekvenci[Noty.nota.A4], 300);
+                    Console.Beep(note.notyNaFrekvenci[Noty.nota.H4], 300);
+                    Console.Beep(note.notyNaFrekvenci[Noty.nota.C5], 300);
+                    break;
+                case "Info":
+                    Console.Clear();
+                    Console.WriteLine("Vítejte v aplikaci Beeper.");
+                    Console.ReadKey();
+                    return false;
+                case "Odejít":
+                    return true;
+            }
+            return false;
         }
     }
 }
