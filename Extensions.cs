44ed34118214df50f594a98aa17639be2e20d343
Voke <Vokeko@MEOWWARE>
2022-08-12 21:39:16 +0200

    Extension - Next a Prev u enumů

---
+++ Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Beeper
+{
+    public static class Extensions
+    {
+        public static T Next<T>(this T src) where T : struct
+        {
+            if (!typeof(T).IsEnum) throw new ArgumentException(String.Format("Argument {0} není Enum!", typeof(T).FullName));
+
+            T[] Arr = (T[])Enum.GetValues(src.GetType());
+            int j = Array.IndexOf<T>(Arr, src) + 1;
+            return (Arr.Length == j) ? Arr[j - 1] : Arr[j];
+        }
+        public static T Prev<T>(this T src) where T : struct
+        {
+            if (!typeof(T).IsEnum) throw new ArgumentException(String.Format("Argument {0} není Enum!", typeof(T).FullName));
+
+            T[] Arr = (T[])Enum.GetValues(src.GetType());
+            int j = Array.IndexOf<T>(Arr, src) - 1;
+            return (j == -1) ? Arr[0] : Arr[j];
+        }
+    }
+}
